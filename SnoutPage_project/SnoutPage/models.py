99b891056e451683125465c6c8f29451fcb656e5
BozhanaKoleva <35367937+BozhanaKoleva@users.noreply.github.com>
2018-03-13 13:32:32 +0000

    add category view and a draft for 'post' model

--- SnoutPage_project/SnoutPage/models.py
+++ SnoutPage_project/SnoutPage/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# A draft for models
+
+class Category(models.Model):
+    name = models.CharField(max_length=18, unique=True) 
+    class Meta:
+        verbose_name_plural = 'Categories'
+        def __str__(self):
+            return self.name
+
+class Post(models.Model):
+    category = models.ForeignKey(Category) 
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=300, blank=True)
+    comments = models.CharField(max_length=300, default=0)
+    tags = models.CharField(max_length=30, blank=True)
+    picture = models.ImageField(upload_to='post_images', blank=True) 
+    likes = models.IntegerField(default=0) 
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.title
+
+
