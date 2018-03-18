1850f093a6d096e43f52450951ca70b639f64766
Ignas Knizikevicius <2268993k@student.gla.ac.uk>
2018-03-18 16:05:11 +0000

    some forms

--- SnoutPage_project/SnoutPage/forms.py
+++ SnoutPage_project/SnoutPage/forms.py
@@ -15,3 +15,46 @@ class UserProfileForm(forms.ModelForm):
         model = UserProfile
         fields = ('picture',)
         
+class PostForm(forms.ModelForm):
+    title = forms.CharField(max_length=default,
+    help_text="Please enter the title of the post.")
+    description = forms.CharField(max_length=300, blank=True,
+    help_text="Please enter the description of the post.")
+    tags = forms.CharField (max_length=30, blank=True,
+    help_text="Add a tag.")
+    picture = forms.ImageField(upload_to='post_images', blank=True)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    class Meta:
+        model = Post
+        fields = ('title', 'description', 'tags', 'picture',)
+
+        
+class PetForm(forms.ModelForm):
+    class Meta:
+        model = Pet
+        fields = ('name', 'picture', 'description')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('description')
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=default,
+    help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200,
+    help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+            return cleaned_data
+
+    class Meta:
+        model = Page
+        fields = ('title', 'url', 'views')
